aef7ef74d554075f90081d0f266b0eb5d2825ee9
Aravinthan333 <aravinthan2606@gmail.com>
2024-10-15 02:52:44 +0530

    "Created header"

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,30 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import AppLayout from "./layouts/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <AppLayout>
+              <Home />
+            </AppLayout>
+          }
+        />
         <Route path="/register" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/profile"
+          element={
+            <AppLayout>
+              <Profile />
+            </AppLayout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,61 @@
+import { BiSolidHome } from "react-icons/bi";
+import { BsFillPersonFill } from "react-icons/bs";
+import { FaPowerOff } from "react-icons/fa";
+// import { IoChatboxEllipses } from "react-icons/io5";
+import { BsChatDotsFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const AppLayout = ({ child }) => {
+  return (
+    <>
+      <div className="bg-slate-100">
+        <div className="flex justify-between items-center mx-6 mr-12 p-3 pr-6">
+          <h1 className="text-4xl font-bold cursor-pointer">Connect...</h1>
+          <ul className="flex gap-4">
+            <Link to={"/home"}>
+              <li
+                title="Home"
+                className="hover:bg-black hover:text-white hover:rounded-full p-2 text-2xl hover:shadow-md transition-all ease-in-out duration-300"
+              >
+                <BiSolidHome />
+              </li>
+            </Link>
+
+            <Link to="/chat">
+              <li
+                title="Chat"
+                className="hover:bg-black hover:text-white hover:rounded-full p-2 text-2xl hover:shadow-md transition-all ease-in-out duration-500"
+              >
+                {/* <IoChatboxEllipses /> */}
+                <BsChatDotsFill />
+              </li>
+            </Link>
+
+            <Link to="/profile">
+              <li
+                title="Profile"
+                className="hover:bg-black hover:text-white hover:rounded-full p-2 text-2xl hover:shadow-md transition-all ease-in-out duration-300"
+              >
+                <BsFillPersonFill />
+                {/* <BsPersonCircle /> */}
+              </li>
+            </Link>
+
+            <Link to={"/logout"}>
+              <li
+                title="Logout"
+                className="hover:bg-black hover:text-white hover:rounded-full p-2 text-2xl hover:shadow-md transition-all ease-in-out duration-300"
+              >
+                <FaPowerOff />
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+      {child}
+      <h1>Footer</h1>
+    </>
+  );
+};
+
+export default AppLayout;
