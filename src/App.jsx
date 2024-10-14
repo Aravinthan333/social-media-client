6277957008399dce93473fb24867f6534b578a8f
Aravinthan333 <aravinthan2606@gmail.com>
2024-10-15 00:17:38 +0530

    "Added react-router-dom and configured routes for Home, Login, Signup, and Profile pages"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
-    <>
-      <h1 className="text-2xl text-red">Test...</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
